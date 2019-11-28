d86b0d107a4d8d9561c34c7e42ee5ef44967fb67
SergiuChilat <chilatsergiu@gmail.com>
2019-11-28 13:58:55 +0200

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+/*
+*
+* Launch APP
+* 
+*
+* func launchApp
+*   get All items
+*   get Data from server
+*   add Items from Data
+*
+*
+* func getData
+*   fetch
+*   return
+* */
+
+startApp();
+
+function startApp(){
+  getAllItems();
+  for (const item of getData()){
+    addItem(item);
+  }
+}
+
+/*==================================*/
+
+function getAllItems () {
+  const items = document.querySelectorAll('#students li');
+  for (const item of  items){
+    attachClickEvent(item);
+  }
+}
+
+function addItem (item) {
+  const newItem = document.createElement("li");
+  const newContent = document.createTextNode(item.name);
+  newItem.appendChild(newContent);
+  document.getElementById("students").appendChild(newItem);
+  attachClickEvent(newItem);
+}
+
+function attachClickEvent(item){
+  item.addEventListener('click', function (event) {
+    removeItem(item);
+  })
+}
+
+function removeItem (item) {
+  item.remove();
+}
+
+function getData () {
+  return [
+    {
+      name: 'Ion',
+      age: 123
+    },
+    {
+      name: 'Mihai',
+      age: 123
+    }
+  ];
+}
